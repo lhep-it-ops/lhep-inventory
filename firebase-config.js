be198422d3ba885fca9c02baf676995381b61840
Mappo23 <filippo.banti.23@gmail.com>
2025-03-11 08:58:49 +0100

    firebase config up

--- firebase-config.js
+++ firebase-config.js
@@ -2,9 +2,9 @@ const firebaseConfig = {
     apiKey: "AIzaSyDxLWAQqGbCv-YmMRKkQukfptTtHg89q7o",
     authDomain: "inventory-db-lhep.firebaseapp.com",
     projectId: "inventory-db-lhep",
-    storageBucket: "inventory-db-lhep.appspot.com",
-    messagingSenderId: "123456789",
-    appId: "APP_ID"
+    storageBucket: "inventory-db-lhep.firebasestorage.app",
+    messagingSenderId: "820650788464",
+    appId: "1:820650788464:web:e1740a2d06bbe38280d6b5"
 };
 
 const app = firebase.initializeApp(firebaseConfig);
